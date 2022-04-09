1af7eda5cd8b84ba1aa9235fc8d3392fd8cdd2a6
manuel <manuelhpineda2000@gmail.com>
2022-04-09 17:20:05 -0600

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,32 +6,32 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-source-contentful',
     options: {
-      "accessToken": "c1-8uxzXzcVNbkF_5bUF44b_0P3Z6u3hlUgfbWGFf1w",
-      "spaceId": "whnjcboixq5a"
+      'accessToken': 'c1-8uxzXzcVNbkF_5bUF44b_0P3Z6u3hlUgfbWGFf1w',
+      'spaceId': 'whnjcboixq5a'
     }
-  }, "gatsby-plugin-styled-components", {
+  }, 'gatsby-plugin-styled-components', {
     resolve: 'gatsby-plugin-google-analytics',
     options: {
-      "trackingId": "310421785"
+      'trackingId': '310421785'
     }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+  }, 'gatsby-plugin-image', 'gatsby-plugin-react-helmet', 'gatsby-plugin-sitemap', {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      'icon': 'src/images/icon.png'
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  }, 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "images",
-      "path": "./src/images/"
+      'name': 'images',
+      'path': './src/images/'
     },
-    __key: "images"
+    __key: 'images'
   }, {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "pages",
-      "path": "./src/pages/"
+      'name': 'pages',
+      'path': './src/pages/'
     },
-    __key: "pages"
+    __key: 'pages'
   }]
 };
