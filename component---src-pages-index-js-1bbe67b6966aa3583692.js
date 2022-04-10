4cf2cfd01816d42a8447cd6d56775676b88aa95c
manuel <manuelhpineda2000@gmail.com>
2022-04-10 10:35:35 -0600

    Updates

--- component---src-pages-index-js-1bbe67b6966aa3583692.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[678],{6558:function(e,l,n){n.r(l);var t=n(7294);l.default=function(){return t.createElement("main",null,t.createElement("h1",null,"Hello world"))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-1bbe67b6966aa3583692.js.map
